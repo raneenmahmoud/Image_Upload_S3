6d9ecd4b5f539ecaa3ec4d77bf1dce887bdd4bed
Raneen Mahmoud <raneenm@gmail.com>
2023-03-15 06:22:32 +0200

    Edit Feature

--- Views/upload.php
+++ Views/upload.php
@@ -35,6 +35,7 @@
     <br>
     <input type="submit" value="Upload Image" name="submit" class="btn bg-body-secondary" style="width:10vw;margin:auto">
  </form>
+</div>
 <script
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
